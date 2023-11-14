9225b4aa0727ab25bd498d080a092c95ae96b8b5
Etienne-31 <90270401+Etienne-31@users.noreply.github.com>
2023-11-14 11:03:55 +0100

    Mise en place premier service Springboot et et service découverte

---
+++ MicroServicesDecouverteMS/src/main/java/insa/project/microservicesdecouvertems/MicroServicesDecouverteMsApplication.java
@@ -0,0 +1,13 @@
+package insa.project.microservicesdecouvertems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesDecouverteMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicesDecouverteMsApplication.class, args);
+    }
+
+}
---
+++ MicroServicesDecouverteMS/src/test/java/insa/project/microservicesdecouvertems/MicroServicesDecouverteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package insa.project.microservicesdecouvertems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesDecouverteMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

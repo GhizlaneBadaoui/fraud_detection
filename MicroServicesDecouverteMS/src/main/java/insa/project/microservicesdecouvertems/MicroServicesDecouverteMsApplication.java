f83041527eb0030d8bbd75a0ad9e553d5289c291
Etienne-31 <90270401+Etienne-31@users.noreply.github.com>
2023-12-05 09:48:27 +0100

    Structure calcul stat MS créé

--- MicroServicesDecouverteMS/src/main/java/insa/project/microservicesdecouvertems/MicroServicesDecouverteMsApplication.java
+++ MicroServicesDecouverteMS/src/main/java/insa/project/microservicesdecouvertems/MicroServicesDecouverteMsApplication.java
@@ -2,8 +2,10 @@ package insa.project.microservicesdecouvertems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroServicesDecouverteMsApplication {
 
     public static void main(String[] args) {

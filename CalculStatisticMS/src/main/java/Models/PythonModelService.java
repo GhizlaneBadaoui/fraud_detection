d1ac7704ac4a721d53b614c18a6e497c25628a7e
walidassimi <assimiwalid8@gmail.com>
2024-01-10 15:21:36 +0100

    some changes

--- CalculStatisticMS/src/main/java/Models/PythonModelService.java
+++ CalculStatisticMS/src/main/java/Models/PythonModelService.java
@@ -1,55 +1,27 @@
 package Models;
 
-import jep.Jep;
-import jep.JepConfig;
-import jep.JepException;
-import jep.SubInterpreter;
+import org.python.util.PythonInterpreter;
 
 public class PythonModelService {
-    private SubInterpreter jep;
-    String pathToScript;
-
-
-    public PythonModelService() {
-    }
-
-    public void ModelService() {
-        // Créer une nouvelle instance de SubInterpreter
-        this.jep = new SubInterpreter();
-        // Utilisez le SubInterpreter pour interagir avec Python
-        this.jep.runScript("from main import init");
-
-        // Prepare input data (this would be your Java input converted to Python-compatible data)
-        // For example, if input_data is a list:
-
-        // Call the Python predict function with input data
-        jep.eval("result = init()");
-
-        // Get the result from Python
-        Object result = jep.getValue("result");
-        System.out.println("Prediction result from Python: " + result);
-
-    }
-
-
-    public void usePythonModel() {
-        try {
-            this.jep.runScript(this.pathToScript);
-            // Continuez avec l'entraînement du modèle et la prédiction comme avant
-        } catch (JepException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void close() {
-        if (this.jep != null) {
-            this.jep.close();
-        }
-    }
 
     public static void main(String[] args) {
-        PythonModelService pythonModelService = new PythonModelService();
-        pythonModelService.ModelService();
+        try {
+
+            PythonInterpreter pi = new PythonInterpreter();
+            System.setProperty("python.home", "C:\\Users\\hp\\.m2\\repository\\org\\python\\jython-standalone");
+            pi.exec("import sys");
+            pi.exec("sys.path.append('CalculStatisticMS/src/main/java/Models/main.py')");
+            pi.exec("import pandas");
+            //
+//            pi.exec("from main import init");
+//            pi.exec("result = init()");
+//            pi.exec("print(result)");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 
 
--- CalculStatisticMS/src/main/java/Models/main.py
+++ CalculStatisticMS/src/main/java/Models/main.py
@@ -1,19 +1,20 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
 from sklearn.tree import DecisionTreeClassifier
-import socket
 
-gridSearch = None
-X_train:any
-X_test:any
-y_train:any
-y_test:any
+
+X_train = None
+X_test = None
+y_train= None
+y_test= None
+
 model = DecisionTreeClassifier(random_state=0)
-grid_search:any
+grid_search= None
 
-def init():        
-    file_path = "./clean_data.csv"
+def init():
+    file_path = "CalculStatisticMS/src/main/java/Models/clean_data.csv"
     dataset = pd.read_csv(file_path, sep = ";")
 
     X = dataset.iloc[:, :-1]  # Sélectionner toutes les colonnes sauf la dernière
@@ -23,11 +24,12 @@ def init():
     num_samples = int(len(X)*0.2)
     X, Y = X[:num_samples], Y[:num_samples]
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    print(X_train)
     return "completed"
-    
+
 def train():
     model.fit(X_train,y_train)
-    param_grid = { 
+    param_grid = {
         'max_depth': [5]
     #    'min_samples_split': [2, 5, 10],
     #    'min_samples_leaf': [1, 2, 4],
@@ -39,7 +41,7 @@ def train():
 
     grid_search = GridSearchCV(DecisionTreeClassifier(random_state=0),param_grid=param_grid,cv=5,scoring='accuracy')
     grid_search.fit(X_train,y_train)
-    
+
 def predict_with_confidence(self, X):
     if hasattr(grid_search.best_estimator_, "predict_proba"):
         probabilities = grid_search.best_estimator_.predict_proba(X)
@@ -48,7 +50,7 @@ def predict_with_confidence(self, X):
     else:
         raise ValueError("Ce modèle ne supporte pas la prédiction de probabilités.")
 
-    
+
 def perf(self):
     cv_scores = cross_val_score(grid_search, X_train, y_train, cv=5).mean()
     print("##########################################################\ncross validataion : ",cv_scores)
@@ -56,6 +58,7 @@ def perf(self):
     print("training score : ",grid_search.score(X_train,y_train))
     print("testing score : ",grid_search.score(X_test,y_test))
 
+print(init())
 
 #################### First iteration  ########################
 

65655f2fd2051e96cea24480931ec3ec8ce5a366
walidassimi <assimiwalid8@gmail.com>
2024-01-16 14:44:07 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+from main import FraudDetectionModel
+import requests
+import joblib
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    remote_file_url = "http://127.0.0.1:5000/"
+    local_file_path = "trained_model.joblib"
+
+    try:
+        # Download the remote file and save it locally
+        response = requests.get(remote_file_url)
+
+        if response.status_code == 200:
+            with open(local_file_path, 'wb') as local_file:
+                local_file.write(response.content)
+            model_filename = 'trained_model.joblib'
+            loaded_model = joblib.load(model_filename)
+            return loaded_model.test
+        else:
+            return(f"Failed to download remote file. HTTP Status Code: {response.status_code}")
+    except Exception as e:
+        return(f"An error occurred: {e}")
+
+@app.route('/test')
+def text():
+    url = "http://127.0.0.1:5000/get_text"
+    return f"Requests library version: {requests.__version__}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)

18d40db174cf2b68de75e98aa70cb86d7d4879f3
walidassimi <assimiwalid8@gmail.com>
2024-01-16 18:16:16 +0100

    some changes

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, send_file
+from instagramAPI import extract_data
+
+app = Flask(__name__)
+
+@app.route('/<url>', methods=['GET'])
+def hello(url):
+    likes = extract_data(post_url=url)
+    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

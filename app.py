ce0fc5356526df23890737a6deb1336a25be5fc5
walidassimi <assimiwalid8@gmail.com>
2024-01-18 20:00:25 +0100

    some changes

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 @app.route('/<url>', methods=['GET'])
 def hello(url):
     likes = extract_data(post_url=url)
-    return url
+    return "hello"
     
-
+@app.route('/', methods=['GET'])
+def home():
+    return "hello"
 
 if __name__ == '__main__':
     app.run(debug=True)

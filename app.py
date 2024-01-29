c9065edbf34cbea3b8a02f5193274b23f9a7ccc3
walidassimi <assimiwalid8@gmail.com>
2024-01-29 13:49:04 +0100

    final commit

--- app.py
+++ app.py
@@ -4,19 +4,23 @@ import joblib
 
 app = Flask(__name__)
 
-
+# Instanciate the model (not trained yet) using the dataset
 model = FraudDetectionModel("clean_data.csv")
 
 @app.route('/', methods=['GET'])
-def hello():
+def home():
+    # Train the model
     model.init()
+    # The file name that contains the trained model 
     model_filename = 'saved_model.joblib'
     joblib.dump(model, model_filename)
+    # Send the response
     return send_file(model_filename, as_attachment=True)
 
+# For testing purposes
 @app.route('/test')
 def test():
-    return "hello2"
+    return "hello from the training MS"
 
 
 if __name__ == '__main__':

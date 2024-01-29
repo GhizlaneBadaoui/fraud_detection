6f5a3182df0247dec7e2a5231df00f20d928e34b
walidassimi <assimiwalid8@gmail.com>
2024-01-29 13:43:48 +0100

    final commit

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ model = None
 
 @app.route('/', methods=['GET'])
 def hello():
+    # The url used to get the trained model 
     remote_file_url = "https://mlflaskms.azurewebsites.net/"
+    # the trained model name
     local_file_path = "trained_model.joblib"
 
     try:
@@ -30,20 +32,22 @@ def hello():
         return(f"An error occurred: {e}")
 
 @app.route('/test', methods=['GET'])
-def text():
-    #return jsonify(100,55,20,[["walid",True,2,2],["walid",True,6,2]])
-    return model.test
+def test():
+    return "Message from the trained model : ",model.test
 
+# Entry point to make the prediction
 @app.route('/pred', methods=['POST'])
 def pred():
     data = []
+    # Get the request data
     json_data = request.get_json()
     for i in json_data:
         data.append([ int(v1) if k1 == 'id' else v1 for k1, v1 in i.items()])
-    print(data)
+    # Make the prediction using the model 
     resp = model.predict_with_confidence(data)
     print(resp)
     print(json.dumps(resp))
+    # Send the response
     return json.dumps(resp)
 
 
